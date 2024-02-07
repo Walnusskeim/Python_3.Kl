dcf8b03720347dec6ae890dff5cc1bf8fa11a6da
PROgrammer <97875562+Walnusskeim@users.noreply.github.com>
2024-02-07 15:02:02 +0100

    Dest Dest 1 2 1 2

---
+++ Hü/AktienSim/DestMain.py
@@ -0,0 +1,26 @@
+"""
+A stock simulation with classes
+Maximilian && Phillip
+❤
+29.01.2024
+"""
+
+import asyncio
+import Aktien_GUI
+#import Aktien_Classes
+import Login
+
+"""
+name = "Testosteronus Maximus"
+PL = Player(name, 690000)
+
+Market = []
+Market.append(LowRiskStock("PhillipsFischereiAG", 230, 2))
+Market.append(MedRiskStock("MaximalGerüstbauAG", 500, 10))
+Market.append(HighRiskStock("R&B RechtskanzleiAG", 727, 50))
+Market.append(HighRiskStock("MelvinsIndusties", 500, 50))
+"""
+
+if __name__ == "__main__":
+    Login3232.Loginwindow()
+    login.mainloop()
