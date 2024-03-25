a6e67211247597559982653d6074d7bcf9f7b849
PROgrammer <97875562+Walnusskeim@users.noreply.github.com>
2024-03-25 11:02:34 +0100

    Code prettier frfr

--- Shooting Range/stonks.py
+++ Shooting Range/stonks.py
@@ -19,20 +19,25 @@ import os
 #                     Code                       #
 ##################################################
 
+# Replace the company names with the ones you want to track.
+# You can add more companies by adding more elements to the list.
+companys = ["AAPL", "MSFT", "VLKAF"]
+
 def write_stock_price_to_file():
-    # Replace "AAPL" with the short name of the company you want to track
-    company = yf.Ticker("AAPL")
-    data = company.history(period="1d")
+    for c in companys:
+        company = yf.Ticker(c)
+        data = company.history(period="1d")
 
-    # Get the current price
-    current_price = data['Close'][0]
+        # Get the current price
+        current_price = data['Close'][0]
 
-    # Round the price to 2 decimal places
-    current_price = round(current_price, 2)
+        # Round the price to 2 decimal places
+        current_price = round(current_price, 2)
 
-    # Write into the file
-    with open('stock_price.txt', 'a') as f:
-        f.write(f'{time.ctime()}: {current_price}\n')
+        # Write into the file
+        with open('stock_price.txt', 'a') as f:
+            f.write(f'{time.ctime()} {c}: {current_price}\n')
+            f.write('-----------------------------------\n')
 
 
 # Create the file if it not already exists when the program starts
@@ -42,4 +47,5 @@ if not os.path.exists('stock_price.txt'):
 # Write the stock price to the file every 30 minutes
 while True:
     write_stock_price_to_file()
+    print("Stock price written to file.")
     time.sleep(1800)  # Sleep for 30 minutes
