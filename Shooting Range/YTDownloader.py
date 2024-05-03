3ef384bc8c637bfc239c86fbbc33a50144d8cf7b
PROgrammer <97875562+Walnusskeim@users.noreply.github.com>
2024-05-03 16:05:42 +0200

    User kann jetzt dümmer sein
    User kann jetzt dümmer sein, ohne dass das Skript abstürzt. Perfekt.

--- Shooting Range/YTDownloader.py
+++ Shooting Range/YTDownloader.py
@@ -22,11 +22,14 @@ from tkinter import StringVar
 def download_video(url, path):
     yt = YouTube(url)
     stream = yt.streams.get_highest_resolution()
-    stream.download(path)
+    if path == "":
+        stream.download("C:\\YTDownloader")
+    else:
+        stream.download(path)
     finished_window = ctk.CTk()
-    finished_window.title("Finished")
+    finished_window.title("Download abgeschlossen!")
     finished_window.geometry("200x100")
-    finished = ctk.CTkLabel(finished_window, text="Download finished")
+    finished = ctk.CTkLabel(finished_window, text="Download abgeschlossen!")
     finished.pack()
     finished_window.mainloop()
 
@@ -45,17 +48,25 @@ path_var = StringVar()
 label = ctk.CTkLabel(window, text="YouTube Downloader", font=("Arial", 20, "bold"))
 label.pack(pady=10)
 
+# Create a label for the URL
+url_label = ctk.CTkLabel(window, text="URL:", font=("Arial", 12))
+url_label.pack(pady=1)
+
 # Create an entry for the URL
-url_entry = ctk.CTkEntry(window, width=200, placeholder_text="url", textvariable=url_var)
-url_entry.pack(pady=12, padx=10)
+url_entry = ctk.CTkEntry(window, placeholder_text="url", width =200, textvariable=url_var)
+url_entry.pack(pady=5, padx=10)
+
+# Create a label for the path
+path_label = ctk.CTkLabel(window, text='Pfad mit "\\" \n("C:\\YTDownloader" wenn keiner definiert) :', font=("Arial", 12))
+path_label.pack(pady=1)
 
 # Create an entry for the path
 path_entry = ctk.CTkEntry(window, width=200, placeholder_text="path", textvariable=path_var)
-path_entry.pack(pady=14, padx=10)
+path_entry.pack(pady=5, padx=10)
 
 # Create a button that calls the download_video function when clicked
-download_button = ctk.CTkButton(window, text="Download", command=lambda: download_video(url_var.get(), path_var.get()))
-download_button.pack()
+download_button = ctk.CTkButton(window, text="Download", fg_color="green" , hover_color="#35682b", command=lambda: download_video(url_var.get(), path_var.get()))
+download_button.pack(pady=10)
 
 # Start the GUI
 window.mainloop()
